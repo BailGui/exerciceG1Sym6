887466a272f912dd4005cab3a259b2bee6f4c1d5
BailGui <guillaumebaillieux88@gmail.com>
2024-09-28 12:37:14 +0200

    TryToConnectTheLinks

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,6 @@ import './vendor/bootstrap/dist/css/bootstrap.min.css';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './js/main.js'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
