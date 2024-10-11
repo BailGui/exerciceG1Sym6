b21624c0efb9b4970d5d3bcd6f028bc049b0d16c
Guillaume <guillaumebaillieux88@gmail.com>
2024-10-11 13:57:56 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,10 +13,18 @@ class AppFixtures extends Fixture
     {
         # instanciation d'un User
         $user = new User();
+        $user->setUsername('admin');
+        $user->setUserMail('admin@gmail.com');
+        $user->setRoles('ROLE_ADMIN','ROLE_REDAC', 'ROLE_MODERATOR');
+        $user->setPassword('admin');
+        $user->setUserActive(true);
+        $user->setUserRealName('The Admin');
+
 
         # utilisation du $manager pour mettre le user en mémoire
         $manager->persist($user);
-
+        
+        # envoie à la base de donnée (commit)
         $manager->flush();
     }
 }
