2612a09cbaf9e60a5873d8f84cd5f3535b9ff88c
Guillaume <guillaumebaillieux88@gmail.com>
2024-10-16 13:15:46 +0200

    majFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,24 +6,39 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 # On va hacher les mots de passe
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-# Chargement de faker et création d'un alias nommé Faker
+# Chargement de Faker et création d'un alias nommé Faker
 use Faker\Factory as Faker;
 # on va récupérer notre entité User
 use App\Entity\User;
+# on va récupérer notre entité Post
+use App\Entity\Post;
 
 class AppFixtures extends Fixture
 {
     # attribut contenant le hacher de mot de passe
     private UserPasswordHasherInterface $passwordHasher;
 
+
     # constructeur qui remplit les attributs
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    public function __construct(
+        UserPasswordHasherInterface $passwordHasher,
+
+    )
     {
         # hache le mot de passe
         $this->passwordHasher = $passwordHasher;
     }
+
+    # constructeur qui remplit les attributs
     public function load(ObjectManager $manager): void
     {
+
+    ###
+    # GESTION de USER
+    ###
+        // Création de Faker
+        $faker = Faker::create();
+
         ###
         # Instanciation d'un User Admin
         #
@@ -37,6 +52,10 @@ class AppFixtures extends Fixture
         $user->setPassword($pwdHash);
         $user->setUserActive(true);
         $user->setUserRealName('The Admin !');
+
+        // création/ update d'un tableau contenant
+        // les User qui peuvent écrire un article
+        $users[] = $user;
 
         # Utilisation du $manager pour mettre le
         # User en mémoire
@@ -54,33 +73,41 @@ class AppFixtures extends Fixture
             $user->setPassword($pwdHash);
             $user->setUserActive(true);
             $user->setUserRealName('The Redac '.$i.' !');
+
+            // création/ update d'un tableau contenant
+            // les User qui peuvent écrire un article
+            $users[] = $user;
+
             # Utilisation du $manager pour mettre le
             # User en mémoire
             $manager->persist($user);
         }
 
         ###
-        #Instanciation de 5 Rédateurs
+        # Instanciation de 3 modérateurs
+        #
         for($i = 1; $i <= 3; $i++){
             $user = new User();
-            $user->setUsername('moderator'.$i);
-            $user->setUserMail('moderator'.$i.'@gmail.com');
+            $user->setUsername('modo'.$i);
+            $user->setUserMail('modo'.$i.'@gmail.com');
             $user->setRoles(['ROLE_MODERATOR']);
-            $pwdHash = $this->passwordHasher->hashPassword($user, 'moderator'.$i);
+            $pwdHash = $this->passwordHasher->hashPassword($user, 'modo'.$i);
             $user->setPassword($pwdHash);
             $user->setUserActive(true);
-            $user->setUserRealName('The MODERATOR '.$i.' !');
+            $user->setUserRealName('The Moderator '.$i.' !');
+
+            // création/ update d'un tableau contenant
+            // les User qui peuvent écrire un article
+            $users[] = $user;
+
             # Utilisation du $manager pour mettre le
             # User en mémoire
             $manager->persist($user);
         }
 
 
-    // instanciation de Faker en français
-        $faker = Faker::create('fr_FR');
-
         ###
-        # Instanciation entre 20 et 40 User sans rôle
+        # Instanciation entre 20 et 40 User sans rôles
         # en utilisant Faker
         #
         $hasard = mt_rand(20,40);
@@ -89,6 +116,7 @@ class AppFixtures extends Fixture
             # nom d'utilisateur au hasard commençant par user-1234
             $username = $faker->numerify('user-####');
             $user->setUsername($username);
+            # création d'un mail au hasard
             $mail = $faker->email();
             $user->setUserMail($mail);
             $user->setRoles(['ROLE_USER']);
@@ -99,9 +127,27 @@ class AppFixtures extends Fixture
             # on va activer 1 user sur 3
             $randActive = mt_rand(0,2);
             $user->setUserActive($randActive);
-            $realname = $faker->name();
-            $user->setUserRealName($realname);
+            # Création d'un 'vrai' nom en français
+            $realName = $faker->name();
+            $user->setUserRealName($realName);
+
             $manager->persist($user);
+
+        }
+
+    ###
+    # GESTION de POST
+    ###
+        for($i = 1; $i <= 100; $i++){
+            $post = new Post();
+            // on prend un auteur au hasard
+            $user = array_rand($users);
+            $post->setUser($user);
+            $title = $faker->realTextBetween(20,150);
+            $post->setPostTitle($title);
+
+
+            $manager->persist($post);
         }
 
         # envoie à la base de donnée (commit)
